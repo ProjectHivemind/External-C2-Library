09c1a5d5be85215d6351d7810552f24f5525970c
Im-Adriano <35747937+Im-Adriano@users.noreply.github.com>
2021-04-28 23:00:51 -0400

    Updated to differentiate between windows/linux/meterpreter

--- POCs/metasploit.py
+++ POCs/metasploit.py
@@ -7,7 +7,7 @@ from hivemind import Hivemind, ModuleFunc, ParamTypes, Module, Implant
 
 # Run this in msfconsole
 # load msgrpc ServerHost=192.168.215.140 ServerPort=55553 User=user Pass='password'
-client = MsfRpcClient(password='password', username='user', ssl=False, server='192.168.215.140', port=55553)
+client = MsfRpcClient(password='password', username='user', ssl=False, server='192.168.215.140', port=55554)
 
 hivemind = Hivemind()
 # Set to actual Hivemind server and port
@@ -26,18 +26,15 @@ while True:
     time.sleep(5)
     for k, v in client.sessions.list.items():
         # Might need a more exact way to determine what type it is but it works for now
-        if 'Command shell' == v['desc']:
+        if 'Meterpreter' == v['desc']:
             implant = Implant(v['tunnel_peer'], f'PAYLOAD {v["via_payload"]}', 'Meterpreter', f'EXPLOIT {v["via_exploit"]}', f'MSF {v["type"]}', '1.0')
             implant.add_module(meterpreter_mod)
         elif 'Windows' in v['info']:
             implant = Implant(v['tunnel_peer'], f'PAYLOAD {v["via_payload"]}', 'Windows', f'EXPLOIT {v["via_exploit"]}', f'MSF {v["type"]}', '1.0')
             implant.add_module(windows_mod)
-        elif 'Linux' in v['via_payload']:
+        else:
             implant = Implant(v['tunnel_peer'], f'PAYLOAD {v["via_payload"]}', 'Linux', f'EXPLOIT {v["via_exploit"]}', f'MSF {v["type"]}', '1.0')
             implant.add_module(linux_mod)
-        else:
-            print('Unknown shell type adding to Hivemind but not adding modules')
-            implant = Implant(v['tunnel_peer'], f'PAYLOAD {v["via_payload"]}', 'Unknown', f'EXPLOIT {v["via_exploit"]}', f'MSF {v["type"]}', '1.0')
 
         tmp = hivemind.implant_callback(implant.ip, implant)
         if tmp is not None:
